936b16156fc21e5b940504419393b0c210cecdca
AndresMosquera9005 <andrubjj@gmail.com>
2024-12-20 17:04:12 -0500

    segundo commit se creo las cartas y unas img

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component: InicioComponent
+    },
+    {
+        path: "header", component: HeaderComponent
+    },
+    {
+        path: "producto", component: ProductosComponent
+    }
+];
