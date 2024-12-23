7afec21dace7d0cb027d70c76d61bc22b45712a3
AndresMosquera9005 <andrubjj@gmail.com>
2024-12-23 09:59:27 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { ProductosComponent } from "./components/productos/productos.component";
 import { InicioComponent } from "./components/inicio/inicio.component";
+import { ComprasComponent } from "./components/compras/compras.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InicioComponent, ComprasComponent, ProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { HeaderComponent } from './components/header/header.component';
 import { ProductosComponent } from './components/productos/productos.component';
+import { ComprasComponent } from './components/compras/compras.component';
 
 export const routes: Routes = [
     {
-        path: "", component: InicioComponent
-    },
-    {
-        path: "header", component: HeaderComponent
+        path: "Inicio", component: InicioComponent
     },
     {
         path: "producto", component: ProductosComponent
+    },
+    {
+        path: "compras", component: ComprasComponent
     }
 ];
